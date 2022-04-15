74369081d36208f7dc2c126ea8863cced32447cb
ariadne lewis-towbes <ari@towb.es>
2022-04-15 10:33:19 -0700

    haha, linter gets mad at files named math.h

---
+++ source/internal/remath.h
@@ -0,0 +1,14 @@
+/**
+ * Created on April 15, 2022 by Ariadne Lewis-Towbes
+ *
+ * See redsp/LICENSE for license information.
+*/
+
+#include <type_traits>
+#ifdef redsp_cxx20
+#include <concepts>
+#endif
+
+// add pi, sin, etc impls
+
+
